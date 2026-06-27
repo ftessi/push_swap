258d1d9349339a81c3367fbf63bb2639a3942ede
ftessi <franciscojtessi@gmail.com>
2026-06-27 03:12:34 +0200

    Fixing type

--- Code/push_swap.h
+++ Code/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umutkilicaslan <umutkilicaslan@student.    +#+  +:+       +#+        */
+/*   By: ftessi <ftessi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 17:33:03 by umutkilicas       #+#    #+#             */
-/*   Updated: 2026/06/27 03:06:45 by umutkilicas      ###   ########.fr       */
+/*   Updated: 2026/06/27 03:12:01 by ftessi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ typedef struct s_node
 	bool median;  // if 1 above median if 0 under
 	int cost;     // total cost of ops
 	int disorder; //
-	struct t_node			*target_node;
-	struct t_node			*next;
-	struct t_node			*prev;
+	t_node			*target_node;
+	t_node			*next;
+	t_node			*prev;
 };
 
 typedef struct s_stack_check
