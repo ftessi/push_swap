7f5a256bcf11232f26068f7e80f91aad7c931adf
ftessi <franciscojtessi@gmail.com>
2026-06-27 03:33:15 +0200

    some fixes

--- Code/push_swap.h
+++ Code/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ftessi <ftessi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 17:33:03 by umutkilicas       #+#    #+#             */
-/*   Updated: 2026/06/27 03:12:01 by ftessi           ###   ########.fr       */
+/*   Updated: 2026/06/27 03:33:00 by ftessi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,36 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_node		t_node;
-
-typedef struct s_node
+typedef struct s_stack_node
 {
-	int value;    // the actual value of the input
-	int index;    // index value of the input on right order
-	int pos;      // current positon on the stack
-	bool median;  // if 1 above median if 0 under
-	int cost;     // total cost of ops
-	int disorder; //
-	t_node			*target_node;
-	t_node			*next;
-	t_node			*prev;
-};
+	int						value;
+	// the actual value of the input
+	int						index;
+	// index value of the input on right order
+	int						pos;
+	// current positon on the stack
+	bool					median;
+	// if 1 above median if 0 under
+	int						cost;
+	// total cost of ops
+	double					disorder;
+	// from 0 to 1 and measures how far
+	// away is from being completely ordered
+	t_stack_node			*target;
+	t_stack_node			*next;
+	t_stack_node			*prev;
+}	t_stack_node;
 
 typedef struct s_stack_check
 {
 	struct s_stack_check	*head;
+	// The link for the top on the list
 	struct s_stack_check	*tail;
+	// bottom
 	int						disorder;
+	// again disorder?
 	int						size;
-}   t_stack_check;
+	// how many nodes
+}							t_stack_check;
+
+#endif
